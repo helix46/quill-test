f84b75a866c51f9d79c6c62ef702197028dd8129
helix46 <bryce.abbott@y7mail.com>
2022-12-29 20:47:54 +1000

    quill test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -39,29 +39,28 @@ export class AppComponent {
     });
   };
 
+  private toolbarOptions = [
+    ['bold', 'italic', 'underline', 'strike'], // toggled buttons
+    ['blockquote', 'code-block'],
+
+    [{ header: 1 }, { header: 2 }], // custom button values
+    [{ list: 'ordered' }, { list: 'bullet' }],
+    [{ script: 'sub' }, { script: 'super' }], // superscript/subscript
+    [{ indent: '-1' }, { indent: '+1' }], // outdent/indent
+    [{ direction: 'rtl' }], // text direction
+
+    [{ size: ['small', false, 'large', 'huge'] }], // custom dropdown
+    [{ header: [1, 2, 3, 4, 5, 6, false] }],
+
+    [{ color: [] }, { background: [] }], // dropdown with defaults from theme
+    [{ font: [] }],
+    [{ align: [] }],
+
+    ['clean'], // remove formatting button
+    ['image'],
+  ];
   modules = {
-    toolbar: [
-      ['background'],
-      ['bold'],
-      ['color'],
-      ['font'],
-      ['code'],
-      ['italic'],
-      ['link'],
-      ['size'],
-      ['strike'],
-      ['script'],
-      ['underline'],
-      ['blockquote'],
-      ['header'],
-      ['indent'],
-      ['list'],
-      ['align'],
-      ['direction'],
-      ['code-block'],
-      ['image'],
-      ['video'],
-    ],
+    toolbar: this.toolbarOptions,
     imageHandler: {
       upload: this.upload,
       accepts: ['png', 'jpg', 'jpeg', 'jfif'], // Extensions to allow for images (Optional) | Default - ['jpg', 'jpeg', 'png']
